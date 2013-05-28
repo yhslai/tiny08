b468a9a24c124ce50922eeae356e078894a28b7d
raincole <raincolee@gmail.com>
2013-05-28 20:36:06 +0800

    PreProcessing: remove comments

--- src/main/scala/tiny08/PreProcessor.scala
+++ src/main/scala/tiny08/PreProcessor.scala
@@ -1,7 +1,19 @@
 package tiny08
 
 object PreProcessor {
+
   def preProcess(asm: String): String = {
-    """(?m)^\s*\n""".r.replaceAllIn(asm, "").trim
+    removeBlankLines(
+      removeComments(asm)
+    )
   }
+
+  private def removeBlankLines(s: String) = {
+    """(?m)^\s*\n""".r.replaceAllIn(s, "").trim
+  }
+
+  private def removeComments(s: String) = {
+    """(?m)//.*$""".r.replaceAllIn(s, "")
+  }
+
 }
