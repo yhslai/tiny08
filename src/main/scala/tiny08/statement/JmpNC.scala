3a03808ef8a47ebdb1c0169c5c61eaab5f91b5c0
raincole <raincolee@gmail.com>
2013-05-30 16:45:09 +0800

    Implement JmpNC

--- src/main/scala/tiny08/statement/JmpNC.scala
+++ src/main/scala/tiny08/statement/JmpNC.scala
@@ -1,14 +1,20 @@
 package tiny08.statement
 
 import tiny08.{Machine, Simulator}
+import tiny08.exception.NoSuchLabel
 
 class JmpNC(val address: Int, label: String, val filename: String, val lineNum: Int)
   extends Instruction {
 
   val debugger = false
 
-  def execute(machine: Machine, dummy: Simulator#LabelTable) {
-    ???
+  def execute(machine: Machine, labelTable: Simulator#LabelTable) {
+    if(labelTable.isDefinedAt(label)) {
+      val labelMem = labelTable(label)
+      if(!machine.carryFlag) machine.programCounter = labelMem
+      else machine.programCounter += 2
+    }
+    else throw new NoSuchLabel(label)
   }
 
   override def toString = {
